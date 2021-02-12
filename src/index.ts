de13f74917446d0aca5bfbb2ad41bf82fb99b714
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-12 16:38:40 +0200

    Optional Interface properties.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 16 });
 
+user.set({ name: 'Peter' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
