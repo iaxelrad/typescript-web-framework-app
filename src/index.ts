6b208aeb09316b036584d1c60d291737010fa85d
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-12 22:20:27 +0200

    Eventing System completed - Triggering Event CB's.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 16 });
 
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Save was triggered');
+});
 
-console.log(user);
+user.trigger('save');
+user.trigger('change');
