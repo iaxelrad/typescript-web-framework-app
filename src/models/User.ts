40aae58cffb0de46c53d943c7bb8ff4afde13272
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-12 23:16:15 +0200

    Refactoring with Composition.

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,11 +5,8 @@ interface UserProps {
   age?: number;
 }
 
-type CallBack = () => void; //Creating a Type alias.
 const BASE_URL = 'http://localhost:3000/';
 export class User {
-  events: { [key: string]: CallBack[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -18,23 +15,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update); // Takes second argument data and inserts into first.
-  }
-
-  on(eventName: string, callback: CallBack): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach(callback => {
-      callback();
-    });
   }
 
   fetch(): void {
