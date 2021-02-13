096109bfbdcb6b0b0e01d56c435f22aa3a44862b
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-13 16:56:54 +0200

    An Advanced Generic Constraint

--- src/models/attributes.ts
+++ src/models/attributes.ts
@@ -1,8 +1,11 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  get(propName: string): number | string {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    // <K extends keyof T> - Sets up a generic constraint. K can only be one of the keys of T.
+    // key: K - key can only be of Type K.
+    // T[K] - standard object lookup. returns the value of property K inside T.
+    return this.data[key];
   }
 
   set(update: T): void {
