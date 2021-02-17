0405acec41d289781b9c86c94de13b29d5d54cf7
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-17 21:13:01 +0200

    View Nesting

--- src/Views/View.ts
+++ src/Views/View.ts
@@ -46,6 +46,8 @@ export abstract class View<T extends Model<K>, K> {
     }
   }
 
+  onRender(): void {}
+
   render = (): void => {
     this.parent.innerHTML = '';
 
@@ -53,6 +55,9 @@ export abstract class View<T extends Model<K>, K> {
     templateElement.innerHTML = this.template();
 
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+
+    this.onRender();
 
     this.parent.append(templateElement.content);
   };
