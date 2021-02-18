ff6539953797c312e84550736fa66273a70fbccd
Itamar Axelrad <iaxelrad@gmail.com>
2021-02-18 22:32:31 +0200

    Resetting initial first User.

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,30 @@ import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
 import { UserList } from './views/UserList';
 
-// const user = User.buildUser({ name: 'NAME', age: 20 });
-// const root = document.getElementById('root');
-// if (root) {
-//   const userEdit = new UserEdit(root, user);
+const user = User.buildUser({ name: 'Placeholder Name' });
+const root = document.getElementById('root');
+if (root) {
+  const userEdit = new UserEdit(root, user);
 
-//   userEdit.render();
+  userEdit.render();
 
-//   console.log(userEdit);
-// } else {
-//   throw new Error(' Root element not found');
-// }
+  console.log(userEdit);
+} else {
+  throw new Error(' Root element not found');
+}
 
-const users = new Collection(
-  'http://localhost:3000/users',
-  (json: UserProps) => {
-    return User.buildUser(json);
-  }
-);
+// const users = new Collection(
+//   'http://localhost:3000/users',
+//   (json: UserProps) => {
+//     return User.buildUser(json);
+//   }
+// );
 
-users.on('change', () => {
-  const root = document.getElementById('root');
+// users.on('change', () => {
+//   const root = document.getElementById('root');
 
-  if (root) {
-    new UserList(root, users).render();
-  }
-});
-users.fetch();
+//   if (root) {
+//     new UserList(root, users).render();
+//   }
+// });
+// users.fetch();
